3938d068590c8d45c4028e50915b0b6ac28aeba8
Bhabani Shankar Rath <bhabanishankarr21@gmail.com>
2024-07-31 01:09:50 +0530

    a

--- bp1/src/components/Bodyarea.jsx
+++ bp1/src/components/Bodyarea.jsx
@@ -1,32 +1,36 @@
 import React from "react";
+import amazon from "./src/assets/amazon.png";
+import flipkart from "./src/assets/flipkart.png";
+import heropage from "./src/assets/hero-image.png";
 
 const Bodyarea = () => {
- return (
-  <div className="secondary-div">
-   <div className="text-div">
-    <p className="p1">YOUR FEET DESERVE THE BEST</p>
-    <p className="p2">
-     YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR
-     FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
-    </p>
-    <div className="button-div">
-     <button className="btn-1">Shop Now</button>
-     <button className="btn-2">Category</button>
+  return (
+    <div className="secondary-div">
+      <div className="text-div">
+        <p className="p1">YOUR FEET DESERVE THE BEST</p>
+        <p className="p2">
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+        <div className="button-div">
+          <button className="btn-1">Shop Now</button>
+          <button className="btn-2">Category</button>
+        </div>
+        <div className="web-redirect-logo">
+          <p>Also available on</p>
+          <div className="logos">
+            <img src={amazon} className="amazon-logo" />
+            <img src={flipkart} className="flipkart-logo" />
+          </div>
+        </div>
+      </div>
+      <div></div>
+      <div className="pic-div">
+        <img src={heropage} className="hero-img" />
+      </div>
     </div>
-    <div className="web-redirect-logo">
-     <p>Also available on</p>
-     <div className="logos">
-      <img src="./src/assets/amazon.png" className="amazon-logo" />
-      <img src="./src/assets/flipkart.png" className="flipkart-logo" />
-     </div>
-    </div>
-   </div>
-   <div></div>
-   <div className="pic-div">
-    <img src="./src/assets/hero-image.png" className="hero-img" />
-   </div>
-  </div>
- );
+  );
 };
 
 export default Bodyarea;
--- bp1/src/components/Navigation.jsx
+++ bp1/src/components/Navigation.jsx
@@ -1,24 +1,25 @@
 import React from "react";
+import logo from "./src/assets/brand_logo.png";
 
 const Navigation = () => {
- return (
-  <>
-   <div className="main-container">
-    <nav className="nav-container">
-     <img src="./src/assets/brand_logo.png" />
-     <ul className="nav-url">
-      <li href="#">MENU</li>
-      <li href="#">LOCATION</li>
-      <li href="#">ABOUT</li>
-      <li href="#">CONTACT</li>
-     </ul>
-     <button type="submit" className="login">
-      Login
-     </button>
-    </nav>
-   </div>
-  </>
- );
+  return (
+    <>
+      <div className="main-container">
+        <nav className="nav-container">
+          <img src={logo} />
+          <ul className="nav-url">
+            <li href="#">MENU</li>
+            <li href="#">LOCATION</li>
+            <li href="#">ABOUT</li>
+            <li href="#">CONTACT</li>
+          </ul>
+          <button type="submit" className="login">
+            Login
+          </button>
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default Navigation;
